e16fe40a476585a598c4142a9b3ce62202327cdf
Kayla <kfang87@gmail.com>
2015-04-15 12:41:35 -0700

    added error, abstracted db access

--- epnumos/app.py
+++ epnumos/app.py
@@ -1,7 +1,7 @@
 __author__ = 'Kayla'
 from flask import Flask, redirect, url_for, request, render_template
 from flask import  jsonify
-from py2neo import Graph, Node
+import dbaccess
 import logging, logging.config
 import sys
 
@@ -16,10 +16,16 @@ def index():
             elif(request.form['trait_word']):
                 return redirect(url_for('display_trait_results',trait_word=request.form['trait_word']))
         else:
-            return render_template('index.html',traits=list_traits())
+            traits = dbaccess.get_traits_list()
+            return render_template('index.html',traits=traits)
     except:
         logger.error("PROBLEM: %s", sys.exc_info())
+        return redirect(url_for('error'))
 # For a name, return most popular traits via the people they are associated with
+
+@app.errorhandler(404)
+def error():
+    return render_template('error.html', 404)
 
 @app.route('/about')
 def about():
@@ -28,125 +34,15 @@ def about():
 
 @app.route('/epnomus/api/name/<base_name>', methods=['GET'])
 def display_name_results(base_name):
-    graph = Graph()
-    person_dict = {}
-    attributes_dict = {}
     name_dict = {}
-
-    # Fill in persons dictionary
-    query = ("MATCH (n:Name {{base_name: '{0}'}}) -[r]- (p:Person) -[r2]-(d:Descriptor) -[r3]- (t:Trait) " \
-            "RETURN p.identifier as person_identifier, " \
-            "p.full_name as person_full_name, " \
-            "p.source as person_source, " \
-            "t.word as trait, " \
-            "sum( toFloat(r2.confidence) * toFloat(r3.similarity))as rating " \
-            "ORDER BY p.identifier,rating desc")\
-            .format(base_name)
-    records = graph.cypher.execute(query)
-
-    for r in records:
-
-        person_identifier = r["person_identifier"]
-        person_full_name = r["person_full_name"]
-        person_source = r["person_source"]
-        trait = r["trait"]
-        rating = r["rating"]
-
-        person_value = {}
-
-        # If person exists, retrieve and add to the trait
-        if person_dict.has_key(person_identifier):
-            person_value = person_dict[person_identifier]
-            trait_dict = person_value["traits"]
-            trait_dict[trait] = round(rating,2)
-            person_value["traits"] = trait_dict
-        else: #initialize personal and traits dictionary
-            person_value["personal"] = {"full_name": person_full_name, "source" : person_source }
-            person_value["traits"] = {trait: round(rating,2)}
-
-        person_dict[person_identifier] = person_value
-
-        name_dict["persons"] = person_dict
-
-    # Fill in name's basic attributes
-    name_node = graph.find_one("Name", "base_name", base_name)
-    if name_node:
-
-        attributes_dict["gender"] = name_node.properties["gender"]
-        attributes_dict["origin"] = name_node.properties["origin"]
-        attributes_dict["meaning"] = name_node.properties["meaning"]
-
-
-    # Fill in name's popularity attribute
-    year_query = ("MATCH (n:Name {{base_name:'{0}'}}) -[r:GIVEN_IN] - (y:Year) " \
-                  "RETURN y.year as year, " \
-                  "r.count as count")\
-                    .format(base_name)
-    popularity_records = graph.cypher.execute(year_query)
-    year_dict = {}
-    for r in popularity_records:
-        year_dict[r["year"]] = r["count"]
-    attributes_dict["popularity"] = year_dict
-
-    # Attach attributes to main name entry
-    name_dict["attributes"] = attributes_dict
-
+    name_dict["persons"] = dbaccess.get_person_dict_for_name(base_name)
+    name_dict["attributes"] = dbaccess.get_attributes_dict_for_name(base_name)
+    name_dict["popularity"] = dbaccess.get_popularity_dict_for_name(base_name)
     return jsonify(name_dict)
 
 @app.route('/epnomus/api/trait/<trait_word>')
 def display_trait_results(trait_word):
-    graph = Graph()
-    name_dict = {}
-
-    query = "MATCH (n:Name) -[r]- (p:Person) - [r2] - (d:Descriptor) -[r3]- (t:Trait {{word:'{}'}}) " \
-            "WHERE length(n.base_name) > 0 " \
-            "return n.base_name as base_name," \
-            "n.origin as origin," \
-            "n.gender as gender," \
-            "n.meaning as meaning," \
-            "p.identifier as person_identifier, " \
-            "p.full_name as person_full_name, " \
-            "p.source as person_source, " \
-            "sum( toFloat(r2.confidence) * toFloat(r3.similarity))as rating " \
-            "ORDER BY rating DESC " \
-            "LIMIT 100".format(trait_word.lower())
-
-    records = graph.cypher.execute(query)
-
-    for r in records:
-        person_identifier = r["person_identifier"]
-        person_full_name = r["person_full_name"]
-        person_source = r["person_source"]
-        rating = r["rating"]
-        basename = r["base_name"]
-        origin = r["origin"]
-        gender = r["gender"]
-        meaning = r["meaning"]
-        person_dict = {}
-        if name_dict.has_key(basename):
-            if name_dict[basename].has_key("persons"):
-                person_dict = name_dict[basename]["persons"]
-        else:
-            name_dict[basename]  = {}
-
-        if not person_dict.has_key(person_identifier):
-            person_dict[person_identifier]  = {"full_name" : person_full_name,
-                         "source" : person_source,
-                         "rating" : round(rating,2)}
-        name_dict[basename]["persons"] = person_dict
-        name_dict[basename]["origin"] = origin
-        name_dict[basename]["gender"] = gender
-        name_dict[basename]["meaning"] = meaning
-    return jsonify(name_dict)
-
-def list_traits():
-    graph = Graph()
-    word_list = map(extract_trait_word,graph.find("Trait"))
-    word_list.sort()
-    return word_list
-
-def extract_trait_word(trait_entry):
-    return trait_entry.properties["word"]
+    return jsonify(dbaccess.get_name_dict_for_trait(trait_word))
 
 if __name__ == '__main__':
     logging.config.fileConfig('logging.ini')
---
+++ epnumos/dbaccess.py
@@ -0,0 +1,117 @@
+__author__ = 'Kayla'
+from py2neo import Graph
+
+graph = Graph()
+
+# Name queries
+
+def get_person_dict_for_name( base_name):
+
+    person_dict = {}
+
+     # Fill in persons dictionary
+    query = ("MATCH (n:Name {{base_name: '{0}'}}) -[r]- (p:Person) -[r2]-(d:Descriptor) -[r3]- (t:Trait) " \
+            "RETURN p.identifier as person_identifier, " \
+            "p.full_name as person_full_name, " \
+            "p.source as person_source, " \
+            "t.word as trait, " \
+            "sum( toFloat(r2.confidence) * toFloat(r3.similarity))as rating " \
+            "ORDER BY p.identifier,rating desc")\
+            .format(base_name)
+    records =  graph.cypher.execute(query)
+    for r in records:
+        person_identifier = r["person_identifier"]
+        person_full_name = r["person_full_name"]
+        person_source = r["person_source"]
+        trait = r["trait"]
+        rating = r["rating"]
+
+        person_value = {}
+
+        # If person exists, retrieve and add to the trait
+        if person_dict.has_key(person_identifier):
+            person_value = person_dict[person_identifier]
+            trait_dict = person_value["traits"]
+            trait_dict[trait] = round(rating,2)
+            person_value["traits"] = trait_dict
+        else: #initialize personal and traits dictionary
+            person_value["personal"] = {"full_name": person_full_name, "source" : person_source }
+            person_value["traits"] = {trait: round(rating,2)}
+
+        person_dict[person_identifier] = person_value
+
+    return person_dict
+
+def get_popularity_dict_for_name(base_name):
+    popularity_dict = {}
+
+    year_query = ("MATCH (n:Name {{base_name:'{0}'}}) -[r:GIVEN_IN] - (y:Year) " \
+              "RETURN y.year as year, " \
+              "r.count as count")\
+                .format(base_name)
+    popularity_records = graph.cypher.execute(year_query)
+
+    for r in popularity_records:
+        popularity_dict[r["year"]] = r["count"]
+
+    return popularity_dict
+
+def get_attributes_dict_for_name(base_name):
+    attributes_dict = {}
+    name_node = graph.find_one("Name", "base_name", base_name)
+    if name_node:
+        attributes_dict["gender"] = name_node.properties["gender"]
+        attributes_dict["origin"] = name_node.properties["origin"]
+        attributes_dict["meaning"] = name_node.properties["meaning"]
+    return attributes_dict
+
+def get_name_dict_for_trait(trait_word):
+    name_dict = {}
+    query = "MATCH (n:Name) -[r]- (p:Person) - [r2] - (d:Descriptor) -[r3]- (t:Trait {{word:'{}'}}) " \
+            "WHERE length(n.base_name) > 0 " \
+            "return n.base_name as base_name," \
+            "n.origin as origin," \
+            "n.gender as gender," \
+            "n.meaning as meaning," \
+            "p.identifier as person_identifier, " \
+            "p.full_name as person_full_name, " \
+            "p.source as person_source, " \
+            "sum( toFloat(r2.confidence) * toFloat(r3.similarity))as rating " \
+            "ORDER BY rating DESC " \
+            "LIMIT 100".format(trait_word.lower())
+
+    records = graph.cypher.execute(query)
+
+    for r in records:
+        person_identifier = r["person_identifier"]
+        person_full_name = r["person_full_name"]
+        person_source = r["person_source"]
+        rating = r["rating"]
+        basename = r["base_name"]
+        origin = r["origin"]
+        gender = r["gender"]
+        meaning = r["meaning"]
+        person_dict = {}
+        if name_dict.has_key(basename):
+            if name_dict[basename].has_key("persons"):
+                person_dict = name_dict[basename]["persons"]
+        else:
+            name_dict[basename]  = {}
+
+        if not person_dict.has_key(person_identifier):
+            person_dict[person_identifier]  = {"full_name" : person_full_name,
+                         "source" : person_source,
+                         "rating" : round(rating,2)}
+        name_dict[basename]["persons"] = person_dict
+        name_dict[basename]["origin"] = origin
+        name_dict[basename]["gender"] = gender
+        name_dict[basename]["meaning"] = meaning
+    return name_dict
+
+def get_traits_list():
+    word_list = map(extract_trait_word,graph.find("Trait"))
+    word_list.sort()
+    return word_list
+
+def extract_trait_word(trait_entry):
+    return trait_entry.properties["word"]
